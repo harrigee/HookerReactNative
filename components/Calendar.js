f708905ab4c01667a7588651dd566f32c18b6a33
Stefan Gregor <stefan.gregor@mantro.net>
2017-02-09 10:29:04 +0100

    first commit

---
+++ components/Calendar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Calendar extends Component {
+
+    render() {
+        return (
+          <View>
+            <Text>Calendar</Text>
+          </View>
+        );
+    }
+}
+
+export default Calendar;
---
+++ index.ios.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Calendar from './components/Calendar'
+
+export default class HookerReactNative extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Calendar/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('HookerReactNative', () => HookerReactNative);
